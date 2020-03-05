cc6c7fe6988f005f863151a3714c74c2aa77e228
Joe Huelsman <kcgooner91@gmail.com>
2020-03-04 20:17:30 -0600

    changed figlet font

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
 const inquirer = require("inquirer");
 const figlet = require("figlet");
+const mysql = require('mysql');
 
-
-async function init() {
+async function figletText() {
     console.log(figlet.textSync('Employee', {
-      font: 'Standard',
+      font: 'speed',
       horizontalLayout: 'default',
       verticalLayout: 'default',
     }));
     console.log(figlet.textSync('Tracker', {
-      font: 'Standard',
+      font: 'speed',
       horizontalLayout: 'default',
       verticalLayout: 'default',
     }));
 };
+figletText();
 
-init();
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Password",
+    database: "companyinfo_db"
+  });
+
+  connection.connect(function(err) {
+    if (err) throw err;
+    // console.log('connected as id ' + connection.threadId);
+});
 
 function run() {
     inquirer.prompt([{
